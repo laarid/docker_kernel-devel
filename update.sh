380817ef144c3f21f8bc0172c328e207fd42c291
You-Sheng Yang <vicamo@gmail.com>
2017-02-09 14:41:11 +0800

    separate architectures

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# set -x
+SUITES="jessie sid"
+
+for f in $(find ${SUITES} -type f -name Dockerfile); do
+  [ -n "$(head -n 1 $f | grep '^#.*GENERATED ')" ] || continue;
+
+  suite=$(echo "$f" | cut -d/ -f1)
+  arch=$(echo "$f" | cut -d/ -f2)
+  cat Dockerfile.template | \
+    sed -e "s,@SUITE@,${suite},g" \
+      -e "s,@ARCH@,${arch},g" > "$f"
+done
